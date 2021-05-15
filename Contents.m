f3cd17952a29012ea84d1d4eb4fb94993fe11b97
Mantas Mikaitis <mantas.mikaitis@manchester.ac.uk>
2021-05-15 14:08:43 +0100

    Added the Contents.m file

---
+++ Contents.m
@@ -0,0 +1,12 @@
+% ANYMATRIX - a MATLAB matrix collection.
+% Version v0.3 15-May-2021
+%
+%   The built-in collection contains 123 matrices organized in 6 groups:
+%
+%       contest  - the CONTEST test matrix toolbox of random matrices
+%                  from networks.
+%       core     - miscellaneous matrices.
+%       gallery  - matrices from the MATLAB gallery.
+%       hadamard - a large collection of Hadamard matrices.
+%       matlab   - other MATLAB matrices (not in gallery).
+%       nessie   - matrices from real-life networks.
