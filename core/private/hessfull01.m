0d4048260bc513a67abc33e288aace4004f35572
Nick Higham <nick.higham@manchester.ac.uk>
2021-06-29 14:19:49 +0100

    Update help for hessfull01.

--- core/private/hessfull01.m
+++ core/private/hessfull01.m
@@ -1,13 +1,17 @@
 function [A,properties] = hessfull01(n)
 %HESSFULL01 Totally nonnegative lower Hessenberg Toeplitz matrix.
 %   HESSFULL01(n) is an n-by-n totally nonnegative lower Hessenberg
-%   Toeplitz matrix. All its eigenvalues are real, and it has floor(n/2)
-%   zero eigenvalues and largest eigenvalue 2*(1+cos(2*pi/(n+2))). The
-%   condition numbers of the largest eigenvalues grow with n.
+%   Toeplitz matrix. All its eigenvalues are real. It has floor(n/2)
+%   zero eigenvalues, which appear in a single Jordan block, and largest
+%   eigenvalue 2*(1+cos(2*pi/(n+2))).
+%   The condition numbers of the largest eigenvalues grow with n.
 
 %   Reference:
 %   Richard Brualdi and Steve Kirkland, Totally nonnegative (0,1)-matrices, 
-%   Linear Algebra Appl. 432, 1650-1662, 2010.
+%      Linear Algebra Appl. 432, 1650-1662, 2010.
+%   Shaun M. Fallat and Michael I. Gekhtman and Charles R. Johnson,
+%     Spectral Structures of Irreducible Totally Nonnegative Matrices,
+%     SIAM J. Matrix Anal. Appl. 22 (2), 627-645, 2000.
 
 properties = {'hessenberg', 'toeplitz', 'nonnegative', ...
               'totally nonnegative', 'integer', 'rank deficient', ...
