61ffad6d4cabad5a0b3bc8c9644c58e94061aa51
Nick Higham <nick.higham@manchester.ac.uk>
2022-06-09 21:16:45 +0100

    Fix typo in help.

--- core/private/pick.m
+++ core/private/pick.m
@@ -2,7 +2,7 @@ function [P,properties] = pick(n)
 %PICK   Skew-symmetric matrix giving equality in Pick's inequality.
 %   PICK(n) is the n-by-n skew-symmetric matrix with zero diagonal,
 %   1s in the upper triangle, and -1s in the lower triangle.
-%   It has eigenvalues sqrt(-1)*cot( (2*j-1)/(2*n)*pi ), j = 1:n.
+%   It has eigenvalues sqrt(-1)*cot( (2*j-1)*pi/(2*n) ), j = 1:n.
 %   It gives equality in Pick's inequality for the imaginary parts of the
 %   eigenvalues of a real matrix.
 
