2de32fb3058be251f3ab050b217f595d50322d75
Nick Higham <nick.higham@manchester.ac.uk>
2022-12-03 17:46:11 +0000

    Tweak comments of core/circul_binom.

--- core/private/circul_binom.m
+++ core/private/circul_binom.m
@@ -1,7 +1,7 @@
 function [A,properties] = circul_binom(n)
 %CIRCUL_BINOM   Circulant matrix based on binomial coefficients.
 %   A = CIRCUL_BINOM(m) is the N-by-N circulant matrix whose first row
-%   has ith element nchoosek(n,i-1).  The eigenvalues are
+%   has ith element nchoosek(n,i-1).  The eigenvalues are the real numbers
 %   (1 + w^i)^n - 1, i=1:n, where w = exp(2*pi*1i/n).
 %   The matrix is singular when n is a multiple of 6, in which case the
 %   null space has dimension 2.
