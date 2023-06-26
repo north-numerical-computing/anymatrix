d8cd242ad4dc0851426e551c28b832b74fcd304f
Mantas Mikaitis <m.mikaitis@leeds.ac.uk>
2023-06-26 11:18:35 +0100

    Test gallery/wilk for n=21 only

--- gallery/private/am_unit_tests.m
+++ gallery/private/am_unit_tests.m
@@ -4,12 +4,6 @@ function test_gallery_neumann(testcase)
 end
 
 function test_gallery_wilk(testcase)
-    A = anymatrix('gallery/wilk',3);
-    anymatrix_check_props(A, 'gallery/wilk', testcase);
-    A = anymatrix('gallery/wilk',4);
-    anymatrix_check_props(A, 'gallery/wilk', testcase);
-    A = anymatrix('gallery/wilk',5);
-    anymatrix_check_props(A, 'gallery/wilk', testcase);
     A = anymatrix('gallery/wilk',21);
     anymatrix_check_props(A, 'gallery/wilk', testcase);
 end
