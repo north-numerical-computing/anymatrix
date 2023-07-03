4b0038e20612fbdb3844d96e66a2916653235bde
Nick Higham <nick.higham@manchester.ac.uk>
2023-07-03 17:59:45 +0100

    Update help.

--- core/private/stoch_revtri.m
+++ core/private/stoch_revtri.m
@@ -2,6 +2,7 @@ function [A,properties] = stoch_revtri(n)
 %STOCH_REVTRI  Reverse triangular stochastic matrix.
 %  A = STOCH_REVTRI(n) is an n-by-n reverse triangular stochastic matrix.
 %  It has eigenvalues 1, -1/2, 1/3, ..., (-1)^(n-1}/n.
+%  This matrix is core/stoch_cesaro with its columns in reverse order.
 
 %  Reference:
 %  Kiran Kedlaya and Lenhard Ng, The Rook and the Half-Chessboard, or How 
